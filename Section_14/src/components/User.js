c9d37d617a9cc82e4d8580a29165aa88ee66813a
Riccardo Fachin <riccardo.fachin@datamars.com>
2025-03-13 08:11:13 +0100

    Section 14

---
+++ Section_14/src/components/User.js
@@ -0,0 +1,20 @@
+import classes from './User.module.css';
+import { Component } from 'react';
+
+class User extends Component {
+  componentWillUnmount() {
+    console.log('User unmounte')
+  }
+
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+/*
+const User = (props) => {
+  return <li className={classes.user}>{props.name}</li>;
+};
+*/
+
+export default User;
